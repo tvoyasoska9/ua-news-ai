81014fe6881a9d820dc255b20aec966683dd6e29
tvoyasoska9 <tvoyasoska9@gmail.com>
2026-09-09 23:16:56 +0200

    Delete app/sources.py

--- app/sources.py
+++
@@ -1,4 +0,0 @@
-RSS_SOURCES = [
-    {"name": "Українська правда", "url": "https://www.pravda.com.ua/rss/", "priority": 10},
-    {"name": "BBC News Україна", "url": "https://feeds.bbci.co.uk/ukrainian/rss.xml", "priority": 8},
-]
